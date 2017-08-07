d9d8bdc4df8d4c0496377b146483f9d34ab58d62
iradriel <30604073+iradriel@users.noreply.github.com>
2017-08-07 22:43:21 +0300

    Update chap4.js

--- chap4.js
+++ chap4.js
@@ -1,6 +1,4 @@
-// Ex.1 Looping a triangle
-
-// The sum of a range
+// Ex.1 The sum of a range
 
 (function() {
     'use strict'
@@ -31,3 +29,36 @@
     console.log(sum(arr));
 
 })()
+
+// Ex.2 Reversing an array
+
+(function(){
+'use strict'
+	function reverseArray(array) {
+		var newArray = [];
+		for (var i = array.length - 1; i >= 0; i--) {
+			newArray.push(array[i]);
+		}
+		return newArray;
+	}
+
+
+	// function reverseArrayInPlace(arr) {
+	// 	var arr = [1, 2, 3, 4, 5]
+	// 	for (var j = arr.length - 2; j <= 0; j--) {
+	// 		arr.splice(arr[j]);
+	// 		arr.push(item);
+	// 		console.log(arr);
+	// 	}
+
+			
+
+	// 	}
+	// 	return arr;
+
+	// }
+
+
+	console.log('reverseArray ' + reverseArray([1, 2, 3, 4, 5, 6]));
+	// console.log('reverseArrayInPlace ' + reverseArrayInPlace([1, 2, 3, 4, 5, 6]));
+})();
